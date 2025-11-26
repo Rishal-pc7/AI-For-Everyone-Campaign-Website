e3f043a83deabc545bab6a0c3da0696e7f78fe34
Rishal-pc7 <rishalpc14@gmail.com>
2025-11-27 00:47:44 +0530

    gitignore file updated

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"87Mpk6Vt2KxYvw4ivI4zkclHZF314v8QN/M+kwoN4gY=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_e40d6c86._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_9e55831b._.js",
-  "static/chunks/node_modules_98a9bea4._.js"
-],
-    source: "dynamic"
-});
